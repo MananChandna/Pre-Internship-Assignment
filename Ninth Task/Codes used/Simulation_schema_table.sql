db3b701ae8f500892d67f41fe98760b915f6ed97
Manan Chandna <chandnamanan2004@gmail.com>
2025-05-05 22:09:58 +0530

    Simulation_schema_table.sql

---
+++ Ninth Task/Codes used/Simulation_schema_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user_products (
+  user_id       INTEGER,
+  product_id    INTEGER,
+  product_name  TEXT,
+  brand         TEXT,
+  category      TEXT,
+  price         NUMERIC,
+  rating        REAL,
+  color         TEXT,
+  size          TEXT
+);
